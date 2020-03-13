371fd6d6b944677e6373a25d9d1718feee37ca74
Tania <klats.usa@gmail.com>
2020-03-13 14:59:13 -0400

    Initial commit

---
+++ app/application.py
@@ -0,0 +1,9 @@
+from pages.main_page import MainPage
+from pages.results_page import ResultsPage
+
+class Application:
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.main_page = MainPage(self.driver)
+        self.results_page = ResultsPage(self.driver)
