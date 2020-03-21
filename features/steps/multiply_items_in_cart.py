da2fa68c07a542c82b67828e3a02cd1f1d506785
Tania <klats.usa@gmail.com>
2020-03-21 14:10:14 -0400

    iframe close fixed

--- features/steps/multiply_items_in_cart.py
+++ features/steps/multiply_items_in_cart.py
@@ -3,13 +3,13 @@ from behave import given, when, then
 
 @when('Return to product search page')
 def twice_back(context):
+    context.app.shopping_cart.close_iframe()
     context.app.shopping_cart.twice_browser_back()
 
 
 @when('On search results page choose another product and click it')
 def new_search_result(context):
     context.app.results_page.first_search_result_open()
-    # context.app.shopping_cart.second_page_first_search_result_open()
 
 
 @then('Expected products would be in cart')
